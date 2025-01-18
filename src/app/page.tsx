1477053a90b372604136e717a9545b9243f9d010
omniflare <namikazesatyam@gmail.com>
2025-01-18 18:07:24 +0530

    feat: init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,12 @@
 "use client";
 
-import Image from "next/image";
-import { ConnectButton } from "thirdweb/react";
+import Example from "@/components/Example";
 import thirdwebIcon from "@public/thirdweb.svg";
-import { client } from "./client";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-      <div className="py-20">
-        <Header />
-
-        <div className="flex justify-center mb-20">
-          <ConnectButton
-            client={client}
-            appMetadata={{
-              name: "Example App",
-              url: "https://example.com",
-            }}
-          />
-        </div>
-
-        <ThirdwebResources />
-      </div>
-    </main>
+    <Example />
   );
 }
 
---
+++ src/components/Example.tsx
@@ -0,0 +1,33 @@
+import { createThirdwebClient } from "thirdweb";
+import { ConnectButton } from "thirdweb/react";
+
+import {
+    inAppWallet,
+    createWallet,
+} from "thirdweb/wallets";
+
+const client = createThirdwebClient({
+    clientId: "....",
+});
+
+const wallets = [
+    inAppWallet({
+        auth: {
+            options: ["email", "x", "passkey", "phone", "google"],
+        },
+    }),
+    createWallet("io.metamask"),
+    createWallet("com.coinbase.wallet"),
+    createWallet("io.rabby"),
+    createWallet("io.zerion.wallet"),
+];
+
+export default function Example() {
+    return (
+        <ConnectButton
+            client={client}
+            wallets={wallets}
+            connectModal={{ size: "compact" }}
+        />
+    );
+}
